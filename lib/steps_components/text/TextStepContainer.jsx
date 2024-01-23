2bf288679210cf9a6fe2bd0ce9ecca15fd19b896
valabji-wbhr <117309695+valabji-wbhr@users.noreply.github.com>
2024-01-23 19:12:09 +0200

    hideStep option

--- lib/steps_components/text/TextStepContainer.jsx
+++ lib/steps_components/text/TextStepContainer.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const TextStepContainer = styled.div`
   align-items: flex-end;
-  display: ${props => ((props.step.metadata && props.step.metadata.hideStep) ? 'none' : 'flex')};;
+  display: ${props => ((props.hideStep) ? 'none' : 'flex')};;
   justify-content: ${props => (props.user ? 'flex-end' : 'flex-start')};
 `;
 
